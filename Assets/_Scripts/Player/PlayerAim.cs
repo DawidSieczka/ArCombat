7de893d01e15aea92b3a077a5df7efa5dc4dc675
DawidSieczka <dawid.sieczka@pirum-technology.com>
2020-02-15 08:32:25 +0100

    init aim

---
+++ Assets/_Scripts/Player/PlayerAim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    private LineRenderer _line;
+    [SerializeField] private float _reflectDistance = 1f;
+    public Camera camera;
+    private void Start()
+    {
+        _line = GetComponent<LineRenderer>();
+        _line.SetVertexCount(2);
+    }
+
+    private void Update()
+    {
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
+        var screenCenter = new Vector3(x, y,transform.position.z);
+        Ray ray = new Ray(camera.ScreenToWorldPoint(screenCenter), transform.position);
+        var hit = Physics.RaycastAll(transform.position, ray.origin, 100);
+        if (hit !=null)
+        {
+            _line.SetPositions(new[] { transform.position, ray.origin });
+        }
+    }
+}
