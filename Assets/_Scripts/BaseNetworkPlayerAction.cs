23a7b9897f4b21213529591578ba2c638786f03d
DawidSieczka <dawid.sieczka@pirum-technology.com>
2020-12-16 21:41:44 +0100

    save before broken changes

--- Assets/_Scripts/BaseNetworkPlayerAction.cs
+++ Assets/_Scripts/BaseNetworkPlayerAction.cs
@@ -6,7 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviourPun
+public class BaseNetworkPlayerAction : MonoBehaviourPun
 {
 
     bool isChange;
