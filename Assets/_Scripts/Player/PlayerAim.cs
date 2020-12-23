a3b740f15cad8a68cc2c60c5b8c27bf7547fad15
DawidSieczka <dawid.sieczka@pirum-technology.com>
2020-12-23 02:01:06 +0100

    working raycast

--- Assets/_Scripts/Player/PlayerAim.cs
+++ Assets/_Scripts/Player/PlayerAim.cs
@@ -1,4 +1,5 @@
 ﻿using Photon.Pun;
+using System;
 using UnityEngine;
 
 public class PlayerAim : MonoBehaviourPun
@@ -18,7 +19,8 @@ public class PlayerAim : MonoBehaviourPun
     private RaycastHit[] hits;
     private Renderer _aimingLineShaderRenderer;
     private SideDetector _sideDetector;
-
+    private PlayerCameraCenter _playerCameraCenter;
+    private const int _groundLayer = 11;
     private void Awake()
     {
         //spawn _aimingLine
@@ -27,6 +29,7 @@ public class PlayerAim : MonoBehaviourPun
             _aimingLine = Instantiate(_aimingLinePrefab, Vector3.zero, Quaternion.identity);
             _aimingLineShaderRenderer = _aimingLine.GetComponent<Renderer>();
             _sideDetector = FindObjectOfType<SideDetector>();
+            _playerCameraCenter = Camera.main.GetComponentInChildren<PlayerCameraCenter>();
         }
     }
 
@@ -40,82 +43,75 @@ public class PlayerAim : MonoBehaviourPun
         _sideDetector.IsZDepthAxis = isZDepthAxis;
     }
 
-    private bool isTargetStandingInTheSameAxis(Vector3 aimedTargetPosition)
-    {
-        var _acceptableDifference = 0.5f;
-        if (isZDepthAxis)
-            return (Mathf.Abs(transform.position.z - aimedTargetPosition.z) < _acceptableDifference);
-        else
-            return (Mathf.Abs(transform.position.x - aimedTargetPosition.x) < _acceptableDifference);
-    }
-
-    public void SetIsPlayerPointingTarget(GameObject aimedTarget, Vector3 aimingPoint)
-    {
-        if (aimedTarget != null && isTargetStandingInTheSameAxis(aimedTarget.transform.position))
-        {
-            aimedTargetPosition = new Vector3(aimedTarget.transform.position.x, aimingPoint.y, aimedTarget.transform.position.z);
-            _isPointingAtTarget = true;
-            _aimingLine.SetActive(true);
-        }
-        else
-        {
-            aimedTargetPosition = null;
-            _isPointingAtTarget = false;
-            _aimingLine.SetActive(false);
-        }
-    }
-
     private void Update()
     {
         if (_aimingLine == null)
             return;
-        hits = Physics.RaycastAll(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), Mathf.Infinity, _layerMask); //wsadzic do kamery, dodac interface'y, dodac event na podstawie trafienia
-        var isRaycastHitting = (hits.Length > 0);
-        if (!isRaycastHitting)
+
+        SetAimingPoint();
+        SetAimingLine();
+    }
+
+    private void SetAimingLine()
+    {
+        _playerPos = transform.position;
+        var _xAxisMiddlePoint = (aimedTargetPosition.Value.x - _playerPos.x) / 2;
+        var _zAxisMiddlePoint = (aimedTargetPosition.Value.z - _playerPos.z) / 2;
+        var _yAxisMiddlePoint = (aimedTargetPosition.Value.y - _playerPos.y) / 2;
+
+        var _direction = (transform.position - aimedTargetPosition.Value);
+        var scaleY = Vector3.Distance(aimedTargetPosition.Value, _playerPos) * 0.5f;
+        _aimingLineShaderRenderer.material.SetFloat("_distanceScaleValue", scaleY);
+
+        if (isZDepthAxis)
         {
-            aimedTargetPosition = null;
-            _aimingLine.SetActive(false);
-            return;
+            float angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg - 90;
+            _aimingLine.transform.position = new Vector3(_playerPos.x + _xAxisMiddlePoint, _playerPos.y + _yAxisMiddlePoint, _playerPos.z);
+            _aimingLine.transform.eulerAngles = Vector3.forward * angle;
         }
         else
         {
-            SetIsPlayerPointingTarget(hits[0].collider.gameObject, hits[0].point);
-            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward) * hits[0].distance, Color.yellow);
+            float angle = Mathf.Atan2(_direction.y, _direction.z) * Mathf.Rad2Deg - 90;
+            _aimingLine.transform.position = new Vector3(_playerPos.x, _playerPos.y + _yAxisMiddlePoint, _playerPos.z + _zAxisMiddlePoint);
+            _aimingLine.transform.eulerAngles = Vector3.left * angle;
         }
 
-        if (_isPointingAtTarget)
+        _aimingLine.transform.localScale = new Vector3(_aimingLine.transform.localScale.x, scaleY, _aimingLine.transform.localScale.z);
+    }
+
+    private void SetAimingPoint()
+    {
+        try
         {
-            //calculate middle position between player and middle of the screen
-            _playerPos = transform.position;
-            var _xAxisMiddlePoint = (aimedTargetPosition.Value.x - _playerPos.x) / 2;
-            var _zAxisMiddlePoint = (aimedTargetPosition.Value.z - _playerPos.z) / 2;
-            var _yAxisMiddlePoint = (aimedTargetPosition.Value.y - _playerPos.y) / 2;
+            _playerCameraCenter.SetDepthToPlayersPosition(isZDepthAxis, transform.position.x, transform.position.z);
+            var pointingDirection = _playerCameraCenter.transform.position;
 
-            //
-            //  Rotate aiming line and set _aimingLine between target and player
-            //
-
-            var _direction = (transform.position - aimedTargetPosition.Value);
-            var scaleY = Vector3.Distance(aimedTargetPosition.Value, _playerPos) * 0.5f;
-            _aimingLineShaderRenderer.material.SetFloat("_distanceScaleValue", scaleY);
-
-            if (isZDepthAxis)
+            Vector3 raycastDirection = GetNormalizedRaycastDirection(pointingDirection);
+            Debug.DrawRay(transform.position, raycastDirection, Color.green);
+            var rayDistance = Vector3.Distance(pointingDirection, transform.position);
+            var isHit = Physics.Raycast(transform.position, raycastDirection, out RaycastHit hit, rayDistance);
+            print(Vector3.Distance(hit.point, pointingDirection));
+            if (isHit && hit.collider.gameObject.layer != _groundLayer)
             {
-                float angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg - 90;
-                _aimingLine.transform.position = new Vector3(_playerPos.x + _xAxisMiddlePoint, _playerPos.y + _yAxisMiddlePoint, _playerPos.z);
-                _aimingLine.transform.eulerAngles = Vector3.forward * angle;
+                Debug.Log("Object has been hit");
+                aimedTargetPosition = hit.point;
             }
             else
             {
-                float angle = Mathf.Atan2(_direction.y, _direction.z) * Mathf.Rad2Deg - 90;
-                _aimingLine.transform.position = new Vector3(_playerPos.x, _playerPos.y + _yAxisMiddlePoint, _playerPos.z + _zAxisMiddlePoint);
-                _aimingLine.transform.eulerAngles = Vector3.left * angle;
+                Debug.Log("Nothing got hit");
+                aimedTargetPosition = pointingDirection;
             }
-
-            _aimingLine.transform.localScale = new Vector3(_aimingLine.transform.localScale.x, scaleY, _aimingLine.transform.localScale.z);
-            //
-            //  Getting collisions on the aiming line way
-            //
         }
+        catch(Exception ex)
+        {
+            Debug.LogError(ex.Message);
+        }
+    }
+    private Vector3 GetNormalizedRaycastDirection(Vector3 pointingDirection)
+    {
+        var x = pointingDirection.x - transform.position.x;
+        var y = pointingDirection.y - transform.position.y;
+        var z = pointingDirection.z - transform.position.z;
+        return (new Vector3(x, y, z)).normalized;
     }
 }
---
+++ Assets/_Scripts/Player/PlayerCameraCenter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraCenter : MonoBehaviour
+{
+    public void SetDepthToPlayersPosition(bool isZDepthAxis, float xDepth, float zDepth)
+    {
+        transform.localPosition = new Vector3(0, 0, transform.localPosition.z);
+
+        if (isZDepthAxis)
+            transform.position = new Vector3(transform.position.x, transform.position.y, zDepth);
+        else
+            transform.position = new Vector3(xDepth, transform.position.y, transform.position.z);
+    }
+}
