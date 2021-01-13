2bdcfbed0a7559f7f0975412e3cc43358c4ea5af
DawidSieczka <dawid.sieczka@pirum-technology.com>
2021-01-13 17:37:48 +0100

    added leaving

--- Assets/_Scripts/Managers/Rooms/CurrentRoomManager.cs
+++ Assets/_Scripts/Managers/Rooms/CurrentRoomManager.cs
@@ -2,15 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CurrentRoomManager : MonoBehaviourPunCallbacks
 {
-
+    int sceneArenaId = 1;
+    int sceneMenuId = 0;
     public void OnClick_StartPlaying()
     {
         //TODO Load map based on scene id - needed menu to choice map for current room
-        int sceneArenaId = 1;
-
         if (PhotonNetwork.IsMasterClient)
         {
             PhotonNetwork.CurrentRoom.IsVisible = false;
@@ -22,5 +22,16 @@ public class CurrentRoomManager : MonoBehaviourPunCallbacks
     public void OnClick_LeaveRoom()
     {
         PhotonNetwork.LeaveRoom();
+
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                PhotonNetwork.CurrentRoom.IsVisible = true;
+                PhotonNetwork.CurrentRoom.IsOpen = true;
+                PhotonNetwork.LoadLevel(sceneMenuId);
+            }
+        }
     }
+
 }
